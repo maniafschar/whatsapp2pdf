b3375bc9ec2f7ac3cbede04ea62cf770831b097f
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-06-12 10:00:31 +0200

    Create WordCloudService.java

---
+++ src/main/java/com/jq/wa2pdf/service/WordCloudService.java
@@ -0,0 +1,50 @@
+package com.jq.wa2pdf.service;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.jq.wa2pdf.service.PdfService.Statistics;
+
+@Service
+public class WordCloudService {
+	public static class Token {
+		int count = 1;
+		final String token;
+
+		private Token(String s) {
+			token = s;
+		}
+	}
+
+	public List<Token> extract(final String text) {
+		String s = text.replaceAll("[ \t\r\n\\,\\.\\-\\!\\?\\[\\]\\{\\}';:/\\(\\)0-9]", " ");
+		while (s.contains("	"))
+			s = s.replaceAll("	", " ");
+		final List<Token> list = new ArrayList<>();
+		final List<String> stopWords = Arrays.asList(IOUtils.toString(getClass().getResourceAsStream("/stopWords.txt"), StandardCharsets.UTF_8).split("\n"));
+		for (s : s.toLowerCase().split(" ")) {
+			Token t = list.stream().filter(e -> e.token.equals(s)).findFirst().orElse(null);
+			if (t != null)
+				t.count++;
+			else if (!stopWords.contains(s))
+				list.add(new Token(s));
+		}
+		list.sort((e, e2) -> (int) Math.signum(e2.count - e.count));
+	}
+}
