209ae0f2c0b6e640ec626aede2c93aadadab926d
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-06-16 10:17:41 +0200

    Create ChartService.java

---
+++ src/main/java/com/jq/wa2pdf/service/ChartService.java
@@ -0,0 +1,12 @@
+package com.jq.wa2pdf.service;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChartService {
+	public void createImage(final List<Statistics> data, final Path file) throws IOException {
+	}
+}
