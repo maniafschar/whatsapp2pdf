d2d7dc740f8f0a43043f9890124e5bf2e4ac36c2
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-06-17 07:28:58 +0200

    Update WordCloudService.java

--- src/main/java/com/jq/wa2pdf/service/WordCloudService.java
+++ src/main/java/com/jq/wa2pdf/service/WordCloudService.java
@@ -241,10 +241,7 @@ public class WordCloudService {
 				w2 = position.width;
 				h2 = position.height;
 			}
-			return (x2 + w2 < x2 || x2 + w2 > x1) &&
-					(y2 + h2 < y2 || y2 + h2 > y1) &&
-					(x1 + w1 < x1 || x1 + w1 > x2) &&
-					(y1 + h1 < y1 || y1 + h1 > y2);
+			return x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2;
 		}
 
 		@Override
