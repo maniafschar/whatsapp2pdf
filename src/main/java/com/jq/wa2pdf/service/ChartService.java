a4c4e6882935b4e32b8b01a709dcc76f3b52a554
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-06-17 08:02:49 +0200

    Update ChartService.java

--- src/main/java/com/jq/wa2pdf/service/ChartService.java
+++ src/main/java/com/jq/wa2pdf/service/ChartService.java
@@ -8,7 +8,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.imageio.ImageIO;
 
@@ -28,43 +30,20 @@ import com.jq.wa2pdf.service.PdfService.Statistics;
 @Service
 public class ChartService {
 	public void createImage(final List<Statistics> data, final Path file) throws IOException {
-		final XYSeriesCollection data2 = new XYSeriesCollection();
-		data.stream().forEach(e -> {
-			try {
-				data2.getSeries(e.user);
-			} catch (UnknownKeyException ex) {
-				data2.addSeries(new XYSeries(e.user));
-			}
-			final XYSeries series = data2.getSeries(e.user);
-		});
-		final CategoryDataset dataset = DatasetUtils.createCategoryDataset("S", "C",
-				new Number[][] { { 1, 2 }, { 2, 8 }, { 3, -6 } });
-		final JFreeChart chart = ChartFactory.createLineChart("Line Chart", "Domain", "Range", dataset);
-		// decorate(chart);
+		final Set<String> users = new HashSet<>();
+		data.stream().forEach(e -> users.add(e.getUser()));
+
 		final BufferedImage image = new BufferedImage(800, 350, BufferedImage.TYPE_4BYTE_ABGR);
-		final Graphics2D g2 = image.createGraphics();
-		chart.draw(g2, new Rectangle2D.Double(0, 0, image.getWidth(), image.getHeight()), null, null);
-		g2.dispose();
+		final Graphics2D g = image.createGraphics();
+		users.stream().forEach(user -> {
+			draw(g, user, data.stream().filter(e -> user.equals(e.getUser())).collect(Collectors.toList()););
+		});
+		g.dispose();
+		image.flush();
 		final File f = file.toAbsolutePath().toFile();
 		ImageIO.write(image, f.getAbsolutePath().substring(f.getAbsolutePath().lastIndexOf('.') + 1), f);
 	}
 
-	private void decorate(final JFreeChart chart) {
-		XYPlot plot = chart.getXYPlot();
-		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-		renderer.setSeriesPaint(0, Color.RED);
-		renderer.setSeriesPaint(1, Color.GREEN);
-		renderer.setSeriesPaint(2, Color.YELLOW);
-		renderer.setSeriesStroke(0, new BasicStroke(4.0f));
-		renderer.setSeriesStroke(1, new BasicStroke(3.0f));
-		renderer.setSeriesStroke(2, new BasicStroke(2.0f));
-		plot.setOutlinePaint(Color.BLUE);
-		plot.setOutlineStroke(new BasicStroke(2.0f));
-		plot.setRenderer(renderer);
-		plot.setBackgroundPaint(Color.DARK_GRAY);
-		plot.setRangeGridlinesVisible(true);
-		plot.setRangeGridlinePaint(Color.BLACK);
-		plot.setDomainGridlinesVisible(true);
-		plot.setDomainGridlinePaint(Color.BLACK);
+	private void draw(final Graphics2D g, final String user, final List<Statistics> data) {
 	}
 }
