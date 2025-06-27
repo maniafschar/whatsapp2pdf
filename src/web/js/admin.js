8e25804209492274773061428dd1deb365f5f505
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-06-27 10:48:27 +0200

    Update admin.js

--- src/web/js/admin.js
+++ src/web/js/admin.js
@@ -21,13 +21,21 @@ class api {
 						s = s.replaceAll(' [[w' + (i + 1) + ']]>', ' style="width:' + widths[i] + '%;">');
 					return s;
 				};
+				var narrowView = window.outerWidth < 700;
 				var s = '<table><thead><tr><th [[w1]]>id</th><th [[w2]]>createdAt</th><th [[w3]]>note</th></tr></thead>';
 				for (var i = 0; i < xhr.tickets.length; i++)
 					s += '<tr><td [[w1]]>' + xhr.tickets[i].id + '</td><td [[w2]]>' + new Date(xhr.tickets[i].createdAt.replace('+00:00', '')).toLocaleString().replace(' ', '&nbsp;') + '</td><td [[w3]]>' + api.sanitizeText(xhr.tickets[i].note) + '<button onclick="api.deleteTicket(event, ' + xhr.tickets[i].id + ')">delete</button></td></tr>';
 				document.querySelector('tickets').innerHTML = replaceWidths([5, 10, 85], s) + '</table>';
-				s = '<table><thead><tr><th [[w1]]>id</th><th [[w2]]>createdAt</th><th [[w3]]>status</th><th [[w4]]>ip</th><th [[w5]]>method</th><th [[w6]]>uri</th><th [[w7]]>query</th><th [[w8]]>time</th><th [[w9]]>body</th><th [[w10]]>referer</th></tr></thead>';
-				for (var i = 0; i < xhr.logs.length; i++)
-					s += '<tr><td [[w1]]>' + xhr.logs[i].id + '</td><td [[w2]]>' + new Date(xhr.logs[i].createdAt.replace('+00:00', '')).toLocaleString().replace(' ', '&nbsp;') + '</td><td [[w3]]>' + xhr.logs[i].status + '</td><td [[w4]]>' + (xhr.logs[i].ip ? '<a href="https://whatismyipaddress.com/ip/' + xhr.logs[i].ip + '" target="sc_ip">' + xhr.logs[i].ip + '</a>' : '') + '</td><td [[w5]]>' + xhr.logs[i].method + '</td><td [[w6]]>' + xhr.logs[i].uri + '</td><td [[w7]]>' + xhr.logs[i].query + '</td><td [[w8]]>' + xhr.logs[i].time + '</td><td [[w9]]>' + api.sanitizeText(xhr.logs[i].body) + '</td><td [[w10]]>' + xhr.logs[i].referer + '</td></tr>';
+				s = '<table><thead><tr><th [[w1]]>id</th><th [[w2]]>createdAt</th><th [[w3]]>status</th><th [[w4]]>ip</th><th [[w5]]>uri</th><th [[w6]]>time</th>';
+				if (!narrowView)
+					s += '<th [[w7]]>body</th><th [[w8]]>referer</th>';
+				s += '</tr></thead>';
+				for (var i = 0; i < xhr.logs.length; i++) {
+					s += '<tr><td [[w1]]>' + xhr.logs[i].id + '</td><td [[w2]]>' + new Date(xhr.logs[i].createdAt.replace('+00:00', '')).toLocaleString().replace(' ', '&nbsp;') + '</td><td [[w3]]>' + xhr.logs[i].status + '</td><td [[w4]]>' + (xhr.logs[i].ip ? '<a href="https://whatismyipaddress.com/ip/' + xhr.logs[i].ip + '" target="sc_ip">' + xhr.logs[i].ip + '</a>' : '') + '</td><td [[w5]]>' + xhr.logs[i].method + ' ' + xhr.logs[i].uri + (xhr.logs[i].query ? '?' + xhr.logs[i].query : '') + '</td><td [[w6]]>' + xhr.logs[i].time + '</td>';
+					if (!narrowView)
+						s += '<td [[w7]]>' + api.sanitizeText(xhr.logs[i].body) + '</td><td [[w8]]>' + xhr.logs[i].referer + '</td>';
+					s += '</tr>';
+				}
 				document.querySelector('logs').innerHTML = replaceWidths([5, 10, 5, 10, 5, 10, 5, 5, 15, 30], s) + '</table>';
 				document.querySelector('msg').innerHTML = xhr.logs.length + ' log entries';
 			}
