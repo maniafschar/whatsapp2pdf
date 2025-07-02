5a7d16f19c35c451be5f3f957ee224267d0a7749
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-07-02 10:47:31 +0200

    Update AdminApi.java

--- src/main/java/com/jq/wa2pdf/api/AdminApi.java
+++ src/main/java/com/jq/wa2pdf/api/AdminApi.java
@@ -2,6 +2,7 @@ package com.jq.wa2pdf.api;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jq.wa2pdf.entity.Log;
 import com.jq.wa2pdf.service.AdminService;
 import com.jq.wa2pdf.service.AdminService.AdminData;
 
@@ -23,6 +26,11 @@ public class AdminApi {
 	@GetMapping("init")
 	public AdminData init() {
 		return this.adminService.init();
+	}
+
+	@GetMapping("log")
+	public List<Log> log(@RequestParam final String search) {
+		return this.adminService.log(search);
 	}
 
 	@PostMapping("build/{type}")
