9b8207054a332234f864394a9e382a30685146c3
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-07-10 16:02:14 +0200

    Update admin.js

--- src/web/js/admin.js
+++ src/web/js/admin.js
@@ -130,7 +130,7 @@ class ui {
 				s += '<label>' + keys[i] + '</label><value>' + ui.sanitizeText(data[keys[i]]) + '</value>';
 		}
 		if (id[0] == 'ticket')
-			s += '<button onclick="api.deleteTicket(event, ' + id[1] + ')">delete</button>';
+			s += '<buttons><button onclick="api.deleteTicket(event, ' + id[1] + ')">delete</button></buttons>';
 		document.querySelector('popup content').setAttribute('i', event.target.getAttribute('i'));
 		ui.popupOpen(s);
 	}
