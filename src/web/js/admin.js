f82f824f1caa998c87af324e7db80212ead85c3f
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-07-12 09:35:44 +0200

    admin table height ios

--- src/web/js/admin.js
+++ src/web/js/admin.js
@@ -241,7 +241,7 @@ class ui {
 }
 window.onresize = function () {
 	var e = document.querySelector('body container logs');
-	e.style.height = 'calc(' + e.parentElement.offsetHeight + 'px - 1.1em)';
+	e.style.height = (e.parentElement.offsetHeight - e.parentElement.children[0].offsetHeight) + 'px';
 	document.querySelectorAll('body container table').forEach(e => {
 		e.style.height = 'calc(' + e.parentElement.offsetHeight + 'px - 1em)';
 	});
