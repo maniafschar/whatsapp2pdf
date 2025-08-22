ead2c76ebeed9274944a1d5ea23a69b0b4b2c2fc
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-08-22 10:54:36 +0200

    Update admin.js

--- src/web/js/admin.js
+++ src/web/js/admin.js
@@ -326,9 +326,9 @@ class ui {
 			var factor = data.sort.indexOf('-asc') > 0 ? 1 : -1;
 			d = d.sort((a, b) => (typeof a[column] == 'string' ? a[column].localeCompare(b[column]) : a[column] - b[column]) * factor);
 		}
-		var isFiltered = function(data) {
+		var isFiltered = function(data, row) {
 			if (data.filter) {
-				var field = d[i][parseInt(data.filter.substring(0, data.filter.indexOf('-'))) + (narrowView ? 1 : 0)];
+				var field = row[parseInt(data.filter.substring(0, data.filter.indexOf('-'))) + (narrowView ? 1 : 0)];
 				var filterValue = data.filter.substring(data.filter.indexOf('-') + 1);
 				return field != filterValue && field.indexOf(filterValue + '<br/>') != 0
 					&& (field.indexOf('<a ') < 0 || field.indexOf('>' + filterValue + '</a>') < 0);
@@ -336,7 +336,7 @@ class ui {
 			return false;
 		};
 		for (var i = 0; i < d.length; i++) {
-			if (!isFiltered(data)) {
+			if (!isFiltered(data, d[i])) {
 				s += '<tr>';
 				for (var i2 = 0; i2 < data.columns.length; i2++) {
 					if (!narrowView || !data.columns[i2].excludeNarrow)
