b4de2b3fd28a9ba970fea25437153dd2246d4781
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-08-25 08:32:22 +0200

    Update api.js

--- src/web/js/api.js
+++ src/web/js/api.js
@@ -196,7 +196,7 @@ class api {
 						tr.classList.add('download');
 					} else {
 						var link = document.createElement('a');
-						link.setAttribute('href', api.url + '/rest/api/pdf/' + document.querySelector('id').innerText) + '/true');
+						link.setAttribute('href', api.url + '/rest/api/pdf/' + document.querySelector('id').innerText + '/true');
 						link.setAttribute('target', '_blank');
 						link.click();
 					}
@@ -276,5 +276,6 @@ class api {
 		xhr.send(param.body);
 	}
 }
+
 
 
