123884195b923590825de780b027a3bce877d345
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-10-13 09:28:06 +0200

    Create PdfService2Test.java

---
+++ src/test/java/com/jq/wa2pdf/service/PdfService2Test.java
@@ -0,0 +1,153 @@
+package com.jq.wa2pdf.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.jq.wa2pdf.TestConfig;
+import com.jq.wa2pdf.WhatsApp2PdfApplication;
+import com.jq.wa2pdf.service.AdminService.AdminData;
+import com.jq.wa2pdf.service.ExtractService.Attributes;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {
+		WhatsApp2PdfApplication.class,
+		TestConfig.class }, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
+				"server.port=9001", "server.servlet.context-path=/rest" })
+@ActiveProfiles("test")
+public class PdfService2Test {
+	@Autowired
+	private PdfService pdfService;
+
+	@Autowired
+	private AdminService adminService;
+
+	private void assertCreation(final String id, final String period) {
+		for (int i = 0; i < 20; i++) {
+			Thread.sleep(500L);
+			final Path path = this.pdfService.get(id, period);
+			if (path != null) {
+				final AdminData adminData = this.adminService.init();
+				if (adminData.getTickets().size() > 0)
+					throw new RuntimeException("PDF creation error\n" +
+							adminData.getTickets().stream().map(e -> e.getNote()).collect(Collectors.joining("\n")));
+				return;
+			}
+		}
+		throw new RuntimeException("PDF creation timed out!");
+	}
+
+	@Test
+	public void create_1() throws Exception {
+		// given
+		final Attributes attributes = this.chartService.analyse(
+				this.getClass().getResourceAsStream("/zip/Chat 1 WhatsApp con abc.zip"), null,
+				UUID.randomUUID().toString());
+
+		// when
+		this.pdfService.create(attributes.id, attributes.getPeriods().get(0).period,
+				attributes.getUsers().get(0).user, null);
+
+		// then
+		assertCreation(attributes.getPeriods().get(0).period, attributes.getUsers().get(0).user);
+	}
+
+	@Test
+	public void create_2() throws Exception {
+		// given
+		final Attributes attributes = this.extractService.analyse(
+				this.getClass().getResourceAsStream("/zip/Chat 2 de WhatsApp con .zip"), null,
+				UUID.randomUUID().toString());
+
+		// when
+		this.pdfService.create(attributes.id, attributes.getPeriods().get(0).period,
+				attributes.getUsers().get(0).user, null);
+
+		// then
+		assertCreation(attributes.getPeriods().get(0).period, attributes.getUsers().get(0).user);
+	}
+
+	@Test
+	public void create_3() throws Exception {
+		// given
+		final Attributes attributes = this.extractService.analyse(
+				this.getClass().getResourceAsStream("/zip/Chat 3 WhatsApp - 9.14_D_CI_8_Pete_87_Germany_.zip"), null,
+				UUID.randomUUID().toString());
+
+		// when
+		this.pdfService.create(attributes.id, attributes.getPeriods().get(0).period,
+				attributes.getUsers().get(0).user, null);
+
+		// then
+		assertCreation(attributes.getPeriods().get(0).period, attributes.getUsers().get(0).user);
+	}
+
+	@Test
+	public void create_4() throws Exception {
+		// given
+		final Attributes attributes = this.extractService.analyse(
+				this.getClass().getResourceAsStream("/zip/Chat 4 WhatsApp - def.zip"), null,
+				UUID.randomUUID().toString());
+
+		// when
+		this.pdfService.create(attributes.id, attributes.getPeriods().get(0).period,
+				attributes.getUsers().get(0).user, null);
+
+		// then
+		assertCreation(attributes.getPeriods().get(0).period, attributes.getUsers().get(0).user);
+	}
+
+	@Test
+	public void create_5() throws Exception {
+		// given
+		final Attributes attributes = this.extractService.analyse(
+				this.getClass().getResourceAsStream("/zip/Chat 5 WhatsApp - Schafkopf.zip"), null,
+				UUID.randomUUID().toString());
+
+		// when
+		this.pdfService.create(attributes.id, attributes.getPeriods().get(0).period,
+				attributes.getUsers().get(0).user, null);
+
+		// then
+		assertCreation(attributes.getPeriods().get(0).period, attributes.getUsers().get(0).user);
+	}
+
+	@Test
+	public void create_6() throws Exception {
+		// given
+		final Attributes attributes = this.extractService.analyse(
+				this.getClass().getResourceAsStream("/zip/Chat 6 WhatsApp with +81 234543 3463.zip"), null,
+				UUID.randomUUID().toString());
+
+		// when
+		this.pdfService.create(attributes.id, attributes.getPeriods().get(0).period,
+				attributes.getUsers().get(0).user, null);
+
+		// then
+		assertCreation(attributes.getPeriods().get(0).period, attributes.getUsers().get(0).user);
+	}
+
+	@Test
+	public void create_7() throws Exception {
+		// given
+		final Attributes attributes = this.extractService.analyse(
+				this.getClass().getResourceAsStream("/zip/Chat 7 WhatsApp with Hab.zip"), null,
+				UUID.randomUUID().toString());
+
+		// when
+		this.pdfService.create(attributes.id, attributes.getPeriods().get(0).period,
+				attributes.getUsers().get(0).user, null);
+
+		// then
+		assertCreation(attributes.getPeriods().get(0).period, attributes.getUsers().get(0).user);
+	}
+}
