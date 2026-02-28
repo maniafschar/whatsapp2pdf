1f98e09e7715233c4dd8779c3c95bf122488054b
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-02-28 08:42:43 +0100

    exclude letters

--- src/web/js/action.js
+++ src/web/js/action.js
@@ -182,9 +182,9 @@ class action {
 		document.getElementsByTagName('upload')[0].style.display = 'none';
 		document.querySelector('attributes button[onclick*="delete"]').style.display = '';
 		document.querySelector('attributes id').innerText = data.id;
-		var s = '<table><tr><th>Period</th><th>Chats</th><th>Words</th><th>Letters</th></tr>';
+		var s = '<table><tr><th>Period</th><th>Chats</th><th>Words</th></tr>';
 		for (var i = 0; i < data.periods.length; i++)
-			s += '<tr value="' + data.periods[i].period + '"><td>' + data.periods[i].period.replace('-\\d', '').replace('\\d/', '').replace('\\d.', '') + '</td><td>' + data.periods[i].chats.toLocaleString() + '</td><td>' + data.periods[i].words.toLocaleString() + '</td><td>' + data.periods[i].letters.toLocaleString() + '</td></tr><tr><td colspan="4"><button onclick="action.preview(event, &quot;' + data.periods[i].period.replaceAll('\\', '\\\\') + '&quot;)">Preview</button></td></tr>';
+			s += '<tr value="' + data.periods[i].period + '"><td>' + data.periods[i].period.replace('-\\d', '').replace('\\d/', '').replace('\\d.', '') + '</td><td>' + data.periods[i].chats.toLocaleString() + '</td><td>' + data.periods[i].words.toLocaleString() + '</td></tr><tr><td colspan="3"><button onclick="action.preview(event, &quot;' + data.periods[i].period.replaceAll('\\', '\\\\') + '&quot;)">Preview</button></td></tr>';
 		document.getElementsByTagName('attributes')[0].querySelector('period').innerHTML = s + '</table><summary>add AI generated summary<br/><span>(data will be sent to Google Gemini)</span></summary>';
 		document.getElementsByTagName('attributes')[0].querySelectorAll('period tr').forEach(tr => {
 			tr.addEventListener('click', () => {
@@ -214,9 +214,9 @@ class action {
 			else
 				e.classList.add('selected');
 		};
-		s = '<table><tr><th>User</th><th>Chats</th><th>Words</th><th>Letters</th></tr>';
+		s = '<table><tr><th>User</th><th>Chats</th><th>Words</th></tr>';
 		for (var i = 0; i < data.users.length; i++)
-			s += '<tr value="' + data.users[i].user + '"' + (s.indexOf('" class="selected">') < 0 ? ' class="selected"' : '') + '><td>' + data.users[i].user + '</td><td>' + data.users[i].chats.toLocaleString() + '</td><td>' + data.users[i].words.toLocaleString() + '</td><td>' + data.users[i].letters.toLocaleString() + '</td></tr>';
+			s += '<tr value="' + data.users[i].user + '"' + (s.indexOf('" class="selected">') < 0 ? ' class="selected"' : '') + '><td>' + data.users[i].user + '</td><td>' + data.users[i].chats.toLocaleString() + '</td><td>' + data.users[i].words.toLocaleString() + '</td></tr>';
 		document.getElementsByTagName('attributes')[0].querySelector('user').innerHTML = s + '</table>';
 		document.getElementsByTagName('attributes')[0].querySelectorAll('user tr').forEach(tr => {
 			tr.addEventListener('click', () => {
